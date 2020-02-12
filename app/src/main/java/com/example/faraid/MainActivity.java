40855b493ce126653a1f5ad278a33f32b9d67c58
Farhan Haq <farhanhaq625@gmail.com>
2020-02-12 09:25:55 +0700

    test1

--- app/src/main/java/com/example/faraid/MainActivity.java
+++ app/src/main/java/com/example/faraid/MainActivity.java
@@ -31,29 +31,39 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.Navstudy:
-                        startActivity(new Intent(getApplicationContext(),FragmentStudyActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.Navkalkulator:
-                        startActivity(new Intent(getApplicationContext(),FragmentKalkulatorActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.Navprofile:
-                        startActivity(new Intent(getApplicationContext(),FragmentProfilActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-
-                return false;
-            }
-        });
-
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FragmentStudyActivity()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.Navstudy:
+                            selectedFragment = new FragmentStudyActivity();
+                            break;
+                        case R.id.Navkalkulator:
+                            selectedFragment = new FragmentKalkulatorActivity();
+                            break;
+                        case R.id.Navprofile:
+                            selectedFragment = new FragmentProfilActivity();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 }
